53f5c992c26a4ccd794a737d581a95490b34288d
cezary-kania <cezary.kaniadev@gmail.com>
2024-03-10 15:21:54 +0100

    Add other use cases for users SignIn integration tests.

--- src/Services/Users/GigiTrucks.Services.Users.Tests.Integration/Identity/SignInTests.cs
+++ src/Services/Users/GigiTrucks.Services.Users.Tests.Integration/Identity/SignInTests.cs
@@ -1,8 +1,8 @@
 ﻿using System.Net;
 using System.Net.Http.Json;
+using System.Text.Json;
 using FluentAssertions;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Time.Testing;
+using GigiTrucks.Services.Users.Core.DTOs;
 using Xunit;
 
 namespace GigiTrucks.Services.Users.Tests.Identity;
@@ -24,5 +24,60 @@ public class SignInTests(UsersApiFactory factory) : IClassFixture<UsersApiFactor
         response.StatusCode
             .Should()
             .Be(HttpStatusCode.BadRequest);
+    }
+    
+    [Fact]
+    public async Task SignIn_ShouldReturnValidationError_WhenPasswordIsEmpty()
+    {
+        // Arrange
+        var requestBody = new { Email = "test@test.com", Password = string.Empty };
+        
+        // Act
+        var response = await _client.PostAsJsonAsync("sign-in", requestBody);
+
+        // Assert
+        response.StatusCode
+            .Should()
+            .Be(HttpStatusCode.BadRequest);
+    }
+    
+    [Fact]
+    public async Task SignIn_ShouldReturnOkStatus_WhenCredentialsAreValid()
+    {
+        // Arrange
+        var credentials = new { Email = "test@test.com", Password = "Test-Password" };
+        await _client.PostAsJsonAsync("sign-up", credentials);
+        
+        // Act
+        var response = await _client.PostAsJsonAsync("sign-in", credentials);
+
+        // Assert
+        response.StatusCode
+            .Should()
+            .Be(HttpStatusCode.OK);
+
+        var responseString = await response.Content.ReadAsStringAsync();
+        var authResult = JsonSerializer.Deserialize<JwtDto>(responseString, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+        authResult.Should().NotBeNull();
+        authResult?.AccessToken.Should().NotBeEmpty();
+        authResult?.UserId.Should().NotBeEmpty();
+    }
+    
+    [Fact]
+    public async Task SignIn_ShouldReturnInternalServerErrorStatus_WhenUserDoesNotExists()
+    {
+        // Arrange
+        var credentials = new { Email = "invalid@test.com", Password = "Test-Password" };
+        
+        // Act
+        var response = await _client.PostAsJsonAsync("sign-in", credentials);
+
+        // Assert
+        response.StatusCode
+            .Should()
+            .Be(HttpStatusCode.InternalServerError);
     }
 }
