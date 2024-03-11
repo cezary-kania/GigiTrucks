6dccbf32fca0b7445da754084ed0f749da967252
cezary-kania <cezary.kaniadev@gmail.com>
2024-03-11 19:54:06 +0100

    Add integration tests for SignUp actions.

---
+++ src/Services/Users/GigiTrucks.Services.Users.Tests.Integration/Identity/SignUpTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using FluentAssertions;
+using Xunit;
+
+namespace GigiTrucks.Services.Users.Tests.Identity;
+
+public class SignUpTests(UsersApiFactory factory) : IClassFixture<UsersApiFactory>
+{
+    private readonly HttpClient _client = factory.CreateClient();
+    
+    [Fact]
+    public async Task SignUp_ShouldReturnNoContentStatus_WhenCredentialsAreValid()
+    {
+        // Arrange
+        var credentials = new { Email = "test@test.com", Password = "Test-Password" };
+        
+        // Act
+        var response = await _client.PostAsJsonAsync("sign-up", credentials);
+
+        // Assert
+        response.StatusCode
+            .Should()
+            .Be(HttpStatusCode.NoContent);
+    }
+    
+    [Fact]
+    public async Task SignUp_ShouldReturnInternalServerErrorStatus_WhenUserAlreadyExists()
+    {
+        // Arrange
+        var credentials = new { Email = "test@test.com", Password = "Test-Password" };
+        await _client.PostAsJsonAsync("sign-up", credentials);
+        
+        // Act
+        var response = await _client.PostAsJsonAsync("sign-up", credentials);
+
+        // Assert
+        response.StatusCode
+            .Should()
+            .Be(HttpStatusCode.InternalServerError);
+    }
+}
