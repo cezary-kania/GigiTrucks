c976315a4e46427bd5f0090b3a170c79d5477913
cezary-kania <cezary.kaniadev@gmail.com>
2024-03-14 19:30:49 +0100

    Add Auditable Entity base class.

---
+++ src/Services/Orders/GigiTrucks.Services.Orders.Domain/Entities/Common/AuditableEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace GigiTrucks.Services.Orders.Domain.Entities.Common;
+
+public abstract class AuditableEntity
+{
+    public DateTimeOffset CreatedAt { get; set; }
+    public Guid? CreatedBy { get; set; }
+    public DateTimeOffset LastModifiedAt { get; set; }
+    public Guid? LastModifiedBy { get; set; }
+}
