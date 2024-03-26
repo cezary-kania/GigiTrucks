6b87ca41d5e7c8b586717a76cf39a457aa068a64
cezary-kania <cezary.kaniadev@gmail.com>
2024-03-26 18:22:00 +0100

    Fix missing postgres connection string in user integration tests pack.

--- src/Services/Users/GigiTrucks.Services.Users.Tests.Integration/UsersApiFactory.cs
+++ src/Services/Users/GigiTrucks.Services.Users.Tests.Integration/UsersApiFactory.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Time.Testing;
 using Testcontainers.PostgreSql;
 using Xunit;
@@ -15,21 +17,26 @@ public class UsersApiFactory : WebApplicationFactory<Program>, IAsyncLifetime
     private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
         .WithImage("postgres:15-alpine")
         .Build();
+    
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        builder.ConfigureHostConfiguration(config =>
+        {
+            config.AddInMemoryCollection(new Dictionary<string, string>
+            {
+                { "ConnectionStrings:Postgres", _dbContainer.GetConnectionString() }
+            }!);
+        });
+        return base.CreateHost(builder);
+    }
+    
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureTestServices(services =>
         {
             var fakeTimeProvider = new FakeTimeProvider();
-            fakeTimeProvider.SetUtcNow(new DateTime(2024, 1, 1));
-            services.AddSingleton(fakeTimeProvider);
-
-            var dbContextOptions = services
-                .SingleOrDefault(s => s.ServiceType == typeof(DbContextOptions<UsersDbContext>));
-            if (dbContextOptions is not null) {
-                services.Remove(dbContextOptions);
-            }
-            services.AddDbContext<UsersDbContext>(options => 
-                options.UseNpgsql(_dbContainer.GetConnectionString()));
+            fakeTimeProvider.SetUtcNow(new DateTime(2024, 1,1));
+            services.AddSingleton(fakeTimeProvider as TimeProvider);
         });
     }
 
