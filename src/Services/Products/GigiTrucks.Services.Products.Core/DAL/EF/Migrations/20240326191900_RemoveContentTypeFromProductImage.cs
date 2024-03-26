001c317c501fadbd1fe2da76bfd524427b89bf96
cezary-kania <cezary.kaniadev@gmail.com>
2024-03-26 20:29:16 +0100

    Remove ContentType from product image entity.

---
+++ src/Services/Products/GigiTrucks.Services.Products.Core/DAL/EF/Migrations/20240326191900_RemoveContentTypeFromProductImage.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GigiTrucks.Services.Products.Core.DAL.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveContentTypeFromProductImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContentType",
+                schema: "products",
+                table: "Images");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContentType",
+                schema: "products",
+                table: "Images",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
