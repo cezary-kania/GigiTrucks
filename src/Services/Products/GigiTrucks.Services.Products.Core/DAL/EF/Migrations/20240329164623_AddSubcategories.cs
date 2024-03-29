d72dd5c749e10e3564599c5a832aa745beadd94b
cezary-kania <cezary.kaniadev@gmail.com>
2024-03-29 17:48:18 +0100

    Add product subcategories.

---
+++ src/Services/Products/GigiTrucks.Services.Products.Core/DAL/EF/Migrations/20240329164623_AddSubcategories.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GigiTrucks.Services.Products.Core.DAL.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSubcategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CategoryId",
+                schema: "products",
+                table: "Categories",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_CategoryId",
+                schema: "products",
+                table: "Categories",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Categories_CategoryId",
+                schema: "products",
+                table: "Categories",
+                column: "CategoryId",
+                principalSchema: "products",
+                principalTable: "Categories",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Categories_CategoryId",
+                schema: "products",
+                table: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_CategoryId",
+                schema: "products",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                schema: "products",
+                table: "Categories");
+        }
+    }
+}
--- src/Services/Products/GigiTrucks.Services.Products.Core/Entities/Category.cs
+++ src/Services/Products/GigiTrucks.Services.Products.Core/Entities/Category.cs
@@ -5,4 +5,5 @@ public class Category
     public Guid Id { get; set; }
     public string Name { get; set; } = null!;
     public ICollection<Product> Products { get; set; } = [];
+    public ICollection<Category> SubCategories { get; set; } = [];
 }
