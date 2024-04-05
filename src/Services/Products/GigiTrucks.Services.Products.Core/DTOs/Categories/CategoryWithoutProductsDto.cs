b96be88351b2510a37d61fd099cfe7d8b831d6b0
cezary-kania <cezary.kaniadev@gmail.com>
2024-04-05 13:15:53 +0200

    Add missing CategoryWithoutProductsDto file in products service.

---
+++ src/Services/Products/GigiTrucks.Services.Products.Core/DTOs/Categories/CategoryWithoutProductsDto.cs
@@ -0,0 +1,6 @@
+﻿namespace GigiTrucks.Services.Products.Core.DTOs.Categories;
+
+public record CategoryWithoutProductsDto(
+    Guid Id,
+    string Name,
+    IEnumerable<CategoryWithoutProductsDto> SubCategories);
