39c752630e266d85e1c697f99899be1678a64b4f
cezary-kania <cezary.kaniadev@gmail.com>
2024-04-17 15:52:09 +0200

    Add FluentValidations to Newsletter service.

--- src/Services/Newsletter/GigiTrucks.Services.Newsletter.Application/DependencyInjection.cs
+++ src/Services/Newsletter/GigiTrucks.Services.Newsletter.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using FluentValidation;
 using GigiTrucks.Services.Common.Identity;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,6 +10,7 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddCurrentUserService();
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         services.AddMediatR(cfg =>
             cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
         return services;
