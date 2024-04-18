41c0dccfc06f480652df63ace5a93ca930d14e8b
cezary-kania <cezary.kaniadev@gmail.com>
2024-04-18 16:33:26 +0200

    Add "Get Cart" action to Cart Service.

--- src/Services/Carts/GigiTrucks.Services.Carts.Api/Program.cs
+++ src/Services/Carts/GigiTrucks.Services.Carts.Api/Program.cs
@@ -4,7 +4,7 @@ using GigiTrucks.Services.Carts.Application.Commands.CreateCart;
 using GigiTrucks.Services.Carts.Application.Commands.DeleteCart;
 using GigiTrucks.Services.Carts.Application.Commands.SubmitCart;
 using GigiTrucks.Services.Carts.Application.Commands.UpdateCart;
-using GigiTrucks.Services.Carts.Application.DTOs;
+using GigiTrucks.Services.Carts.Application.Queries;
 using Mapster;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -17,62 +17,48 @@ var app = builder.Build();
 app.MapGet("/", () => "Carts");
 
 var cartGroup = app.MapGroup("api/cart")
-    .WithName("Cart");
+    .WithTags("Cart");
 
-cartGroup.MapGet("/{cartId:Guid}",
-    (Guid cartId) =>
-    {
-        var mockedResult = new CartDetailsDto
-        (
-            cartId,
-            Guid.NewGuid(),
-            new List<CartItemDto>
-            {
-                new(Guid.NewGuid(), 1)
-            }
-        );
-        return Results.Ok(mockedResult);
-    }
-).WithName("Get Cart");
+cartGroup.MapGet("/{cartId:Guid}", async (
+    [FromRoute] Guid cartId,
+    [FromServices] ISender sender) 
+        => Results.Ok(await sender.Send(new GetCart(cartId))))
+    .WithName("GetCart");
 
 cartGroup.MapPost("/", async (
-        [FromBody] CreateCartRequest request,
-        [FromServices] ISender sender) =>
+    [FromBody] CreateCartRequest request,
+    [FromServices] ISender sender) =>
     {
         var customerId = Guid.NewGuid(); // FIXME: Replace with current user Id
         var createCartCommand = request.Adapt<CreateCart>() with { CustomerId = customerId, CartId = Guid.NewGuid() };
         await sender.Send(createCartCommand);
         return Results.CreatedAtRoute("Get Cart", createCartCommand.CartId);
-    }
-).WithName("Create Cart");
+    }).WithName("CreateCart");
 
 cartGroup.MapPut("/{cartId:Guid}", async (
-        [FromRoute] Guid cartId,
-        [FromBody] UpdateCartRequest request,
-        [FromServices] ISender sender) =>
+    [FromRoute] Guid cartId,
+    [FromBody] UpdateCartRequest request,
+    [FromServices] ISender sender) =>
     {
         var updateCartCommand = request.Adapt<UpdateCart>() with { CartId = cartId };
         await sender.Send(updateCartCommand);
         return Results.NoContent();
-    }
-).WithName("Update Cart");
+    }).WithName("UpdateCart");
 
 cartGroup.MapPatch("/{cartId:Guid}/submit", async (
-        [FromRoute] Guid cartId,
-        [FromServices] ISender sender) =>
+    [FromRoute] Guid cartId,
+    [FromServices] ISender sender) =>
     {
         await sender.Send(new SubmitCart(cartId));
         return Results.NoContent();
-    }
-).WithName("Submit");
+    }).WithName("Submit");
 
 cartGroup.MapDelete("/{cartId:Guid}", async (
-        [FromRoute] Guid cartId,
-        [FromServices] ISender sender) =>
+    [FromRoute] Guid cartId,
+    [FromServices] ISender sender) =>
     {
         await sender.Send(new DeleteCart(cartId));
         return Results.NoContent();
-    }
-).WithName("Delete");
+    }).WithName("Delete");
 
 app.Run();
---
+++ src/Services/Carts/GigiTrucks.Services.Carts.Application/Queries/GetCart.cs
@@ -0,0 +1,6 @@
+﻿using GigiTrucks.Services.Carts.Application.DTOs;
+using MediatR;
+
+namespace GigiTrucks.Services.Carts.Application.Queries;
+
+public class GetCart(Guid cartId) : IRequest<CartDetailsDto>;
