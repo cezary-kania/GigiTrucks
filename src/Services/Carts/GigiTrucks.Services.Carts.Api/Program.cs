8e367814ec4101673ab0f0186ba99c490526814f
cezary-kania <cezary.kaniadev@gmail.com>
2024-04-24 15:09:04 +0200

    Add swagger to cart service.

--- src/Services/Carts/GigiTrucks.Services.Carts.Api/Program.cs
+++ src/Services/Carts/GigiTrucks.Services.Carts.Api/Program.cs
@@ -16,7 +16,18 @@ builder.Services
     .AddApplication()
     .AddInfrastructure(builder.Configuration);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
 
 app.MapGet("/", () => "Carts");
 
