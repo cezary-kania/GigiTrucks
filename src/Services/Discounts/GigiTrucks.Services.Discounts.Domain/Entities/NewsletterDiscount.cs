623ebcd466d27a2b3f67565b98d7b63c3912e07e
cezary-kania <cezary.kaniadev@gmail.com>
2024-05-09 15:05:46 +0200

    Add CustomerId to Newsletter Discounts

--- src/Services/Discounts/GigiTrucks.Services.Discounts.Domain/Entities/NewsletterDiscount.cs
+++ src/Services/Discounts/GigiTrucks.Services.Discounts.Domain/Entities/NewsletterDiscount.cs
@@ -3,4 +3,5 @@ namespace GigiTrucks.Services.Discounts.Domain.Entities;
 
 public class NewsletterDiscount : Discount {
     public bool IsUsed { get; set; }
+    public Guid CustomerId { get; set; }
 }
