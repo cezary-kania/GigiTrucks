76836bfaca7c16bcab6bb079b59adcfccc210dda
cezary-kania <cezary.kaniadev@gmail.com>
2024-05-11 19:55:45 +0200

    Add health checks to products service

--- src/Services/Products/GigiTrucks.Services.Products.Api/Program.cs
+++ src/Services/Products/GigiTrucks.Services.Products.Api/Program.cs
@@ -1,10 +1,16 @@
 using GigiTrucks.Services.Products.Core;
+using GigiTrucks.Services.Products.Health;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCore(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHealthChecks()
+    .AddInfrastructureHealthChecks(builder.Configuration);
 
 var app = builder.Build();
 
@@ -16,6 +22,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseCore();
+
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponseNoExceptionDetails
+});
 
 app.Run();
 
---
+++ src/Services/Products/GigiTrucks.Services.Products.Core/Health/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GigiTrucks.Services.Products.Health;
+
+public static class Extensions
+{
+    public static IHealthChecksBuilder AddInfrastructureHealthChecks(
+        this IHealthChecksBuilder builder,
+        ConfigurationManager configuration)
+    {
+        builder.AddNpgSql(
+            connectionString: configuration.GetConnectionString("Postgres")!,
+            name: "PostgreSQL/Products DB");
+
+        builder.AddAzureBlobStorage(
+            connectionString: configuration["BlobStorageSettings:ConnectionString"]!,
+            name: "Azure Blob Storage");
+        return builder;
+    }
+}
